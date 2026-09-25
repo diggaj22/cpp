c9cc32d8a42d49ebda6801a18736c09a72d802f2
diggaj22 <youdiggaj14@gmail.com>
2026-09-25 21:18:06 +0530

    Create code6.cpp

---
+++ Codebook-1/code6.cpp
@@ -0,0 +1,17 @@
+#include 
+using namespace std;
+
+class Demo {
+public:
+    Demo() {
+        cout << "Constructor called\n";
+    }
+    ~Demo() {
+        cout << "Destructor called\n";
+    }
+};
+
+int main() {
+    Demo d;
+    return 0;
+}
