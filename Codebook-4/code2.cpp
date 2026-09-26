c2a75956d3a6a452c2cc8c47c7264af94db10a79
diggaj22 <youdiggaj14@gmail.com>
2026-09-26 14:26:01 +0530

    Create code2.cpp

---
+++ Codebook-4/code2.cpp
@@ -0,0 +1,24 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main() {
+    std::ifstream inputFile("message.txt");
+
+    if (!inputFile) {
+        std::cerr << "Error: Could not open message.txt\n";
+        return 1;
+    }
+
+    std::string line;
+
+    std::cout << "File Content:\n";
+
+    while (std::getline(inputFile, line)) {
+        std::cout << line << '\n';
+    }
+
+    inputFile.close();
+
+    return 0;
+}
